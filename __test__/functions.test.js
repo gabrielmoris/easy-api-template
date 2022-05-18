04fe67801c2f871b6c0f6bf87344bbbf51272f97
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-05-18 21:00:28 +0200

    add more tests

--- __test__/functions.test.js
+++ __test__/functions.test.js
@@ -6,6 +6,11 @@ describe("/", () => {
         it("should return 200", async () => {
             await supertest(app).get("/").expect(200);
         });
+
+        it("should return a body with a result", async () => {
+            const { body } = await supertest(app).get("/");
+            expect(body).toEqual({ "result": "get in /" });
+        });
     });
 
     describe("post / route", () => {
@@ -16,16 +21,40 @@ describe("/", () => {
                 .expect(200);
         });
 
+         it("should return a body with the message", async () => {
+             const { body } = await supertest(app)
+                 .post("/")
+                 .send("message=Testing with supertest");
+             expect(body).toEqual({
+                 "This is your message": "Testing with supertest",
+             });
+         });
+
         it("should return 400", async () => {
             await supertest(app).post("/").send("nomessage=").expect(400);
+        });
+
+        it("should return a body with an error when it fails", async () => {
+            const { body } = await supertest(app)
+                .post("/")
+                .send("nomessage=");
+            expect(body).toEqual({
+                "Error in /": "Insert key 'message' (your message)",
+            });
         });
     });
 });
 
 describe("/1", () => {
     describe("get /1 route", () => {
+        
         it("should return 200", async () => {
             await supertest(app).get("/1").expect(200);
+        });
+
+         it("should return a body with the result", async () => {
+            const { body } = await supertest(app).get("/1");
+            expect(body).toEqual({ "result": "get in /1" });
         });
     });
 
@@ -37,15 +66,43 @@ describe("/1", () => {
                 .expect(200);
         });
 
+         it("should return a body with the message", async () => {
+             const { body } = await supertest(app)
+                 .post("/1")
+                 .send("message=Testing with supertest");
+             expect(body).toEqual({
+                 "This is your message": "Testing with supertest",
+             });
+         });
+
         it("should return 400", async () => {
             await supertest(app).post("/1").send("nomessage=").expect(400);
         });
+
+        it("should return a body with an error when it fails", async () => {
+            const { body } = await supertest(app).post("/1").send("nomessage=");
+            expect(body).toEqual({
+                "Error in /1": "Insert key 'message' (your message)",
+            });
+        });
     });
 });
+
 describe("/*", () => {
     describe("get /any-other route", () => {
         it("should return 404", async () => {
             await supertest(app).get("/foo").expect(404);
         });
+
+        it("should return a body with {response: 404 This URI doesn't exist} when it fails", async () => {
+            const { body } = await supertest(app)
+                .post("/any-other route")
+                .send("nomessage=");
+            expect(body).toEqual({
+                response: "404 This URI doesn't exist",
+            });
+        });
     });
+
+    
 });
